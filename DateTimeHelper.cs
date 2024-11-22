b5bec952ea82d5d90b6d42dfb2b1d5308e56cb1d
Youssef Salah <145178519+YoussefSalah842@users.noreply.github.com>
2024-11-22 21:55:59 +0200

    Add files via upload

---
+++ DateTimeHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DateTimeHelper
+{
+    public static class DateHelper
+    {
+        
+        public static string FormatDate(DateTime date, string format = "dd-MM-yyyy")
+        {
+            return date.ToString(format);
+        }
+
+        
+        public static DateTime AddDays(DateTime date, int days)
+        {
+            return date.AddDays(days);
+        }
+
+        
+        public static DateTime SubtractDays(DateTime date, int days)
+        {
+            return date.AddDays(-days);
+        }
+
+        
+        public static int GetDaysDifference(DateTime date1, DateTime date2)
+        {
+            return (date2 - date1).Days;
+        }
+
+        
+        public static bool IsValidDate(string dateString, string format = "dd-MM-yyyy")
+        {
+            DateTime parsedDate;
+            return DateTime.TryParseExact(dateString, format, null, System.Globalization.DateTimeStyles.None, out parsedDate);
+        }
+
+        
+        public static DateTime GetCurrentDateTime()
+        {
+            return DateTime.Now;
+        }
+    }
+}
